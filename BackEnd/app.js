7dd20e48f77244f7da06f415dc53cbd0cab046df
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-14 11:55:00 -0700

    Updated

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,3 +4,9 @@ const cors = require('cors');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => { console.log( `Server running on Port ${PORT}`);
+});
+
+module.exports = app;
