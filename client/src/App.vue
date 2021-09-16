d6188b1b469e49173f921b013bcebacba3fe31d9
khoa <54516445+khoatransjsu@users.noreply.github.com>
2021-09-16 15:01:01 -0700

    fix frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,42 +1,65 @@
 <template>
-  <div>
-      <h1> Hello World </h1>
-      <ul>
-        <li v-for="item in data" :key="item.name">
-            {{ item.name }}
-        </li>
-      </ul>
+  <div>  
+        <div>
+            <a href='/'> Home </a>
+        </div>
+
+        <div>
+          <ul>
+            <li v-for="item in data" :key="item.name" v-on:click="openFolder(item.name, item.isFolder)">
+                          {{ item.name }}
+            </li>
+          </ul>
+        </div>
+    
   </div>
 </template>
 
 <script>
 
 import axios from 'axios'
+
 export default {
+
   name: 'App',
   
   data(){
     return {
-       data: []
+       data: [],
+       breadCrumb:'',
+       
     }
   },
-  components: {
+  methods: {
+    async fetch_data(url) {
+        let response = await axios.get(url);
+        this.data = response.data
+
+    },
+    openFolder(filename, isFolder){
+        let currentFolder = [];
+        if (isFolder) {     
+            if (filename == currentFolder[currentFolder.length - 1])
+                return false;
+            if (isFolder == 2) {
+                // get folder name index + 1
+                let changeFolderIndex = currentFolder.indexOf(filename) + 1;
+
+                // if filename = '', go to home folder, else remove any folder after 'changeFolderIndex' from array
+                currentFolder = (filename == '') ? [] : currentFolder.splice(0, changeFolderIndex);
+            }else
+                currentFolder.push(filename);
+
+            this.fetch_data("http://localhost:5000/get/directory?path=" + encodeURIComponent(currentFolder.join("/")));
+        }else{
+          let dir = ((currentFolder.length  > 0) ?  "/" : "") + currentFolder.join("/");
+          console.log(encodeURIComponent(dir + "/" + filename));
+        }
+    } 
     
   },
-  methods: {
-    async fetchData(){
-      try {
-        const response = await axios.get('http://localhost:5000/get/directory');
-        this.data = response.data;
-
-        console.log(this.data);
-      } catch (e) {
-        console.log(e);
-      }
-    },
-  },
   created(){
-    this.fetchData();
+    this.fetch_data('http://localhost:5000/get/directory');
   }
 }
 </script>
